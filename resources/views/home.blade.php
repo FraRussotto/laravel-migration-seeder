b9f49ea7068e763e98bc3e4f899356aa286ee35a
FraRussotto <russottofrancesco1@gmail.com>
2023-11-21 17:25:06 +0100

    [edit] Controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Trains</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    <h1>Elenco treni</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Codice treno</th>
+                <th scope="col">Azienda</th>
+                <th scope="col">Partenza</th>
+                <th scope="col">Arrivo</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
