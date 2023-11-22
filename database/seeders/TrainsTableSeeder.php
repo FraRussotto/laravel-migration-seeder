8b1b173f5361eff6cfc2bd37135303c607912184
FraRussotto <russottofrancesco1@gmail.com>
2023-11-22 19:59:39 +0100

    [edit] Trains seeder

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+
+class create_TrainTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $trains = new Train();
+            $trains->company = $faker->company();
+            $trains->departure_station = $faker->city();
+            $trains->arrival_station = $faker->city();
+            $trains->departure_time = $faker->dateTimeInInterval('+2 day', '+3 day');
+            $trains->arrival_time = $faker->dateTimeInInterval('+2 day', '+3 day');
+            $trains->train_code = $faker->ean8();
+            $trains->carriage = $faker->numberBetween(4, 15);
+            $trains->is_late = false;
+            $trains->is_deleted = false;
+            $trains->save();
+        }
+    }
+}
