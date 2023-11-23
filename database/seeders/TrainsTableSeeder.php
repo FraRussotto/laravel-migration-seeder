4589b65357ca5f07d2a43daeb9c5b0005ec23022
FraRussotto <russottofrancesco1@gmail.com>
2023-11-23 10:09:52 +0100

    [fix] TrainsTableSeeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -8,7 +8,7 @@ use App\Models\Train;
 use Faker\Generator as Faker;
 
 
-class create_TrainTableSeeder extends Seeder
+class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -23,11 +23,12 @@ class create_TrainTableSeeder extends Seeder
             $trains->departure_station = $faker->city();
             $trains->arrival_station = $faker->city();
             $trains->departure_time = $faker->dateTimeInInterval('+2 day', '+3 day');
-            $trains->arrival_time = $faker->dateTimeInInterval('+2 day', '+3 day');
+            $trains->arrival_time = $faker->dateTimeInInterval($trains->departure_time, '+3 days');;
             $trains->train_code = $faker->ean8();
             $trains->carriage = $faker->numberBetween(4, 15);
             $trains->is_late = false;
             $trains->is_deleted = false;
+
             $trains->save();
         }
     }
